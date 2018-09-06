9d7821333d5c7afd8d6ee5a55e014e5f4e860c02
ARTHUR JOEL GARCIA <arthur.ttco@hotmail.com>
2018-09-06 14:04:33 -0300

    Create GeradorDeAlerta.java

---
+++ desafio-fullstack/src/main/java/com/involves/selecao/alerta/GeradorDeAlerta.java
@@ -0,0 +1,22 @@
+package com.involves.selecao.alerta
+
+public class GeradorDeAlerta {
+     private AlertaBuilder bdr;
+     
+     GeradorDeAlerta(){}
+     
+     public Alerta geraAlerta(){
+     
+        return bdr.doPondoDeVenda()
+         comDescricao ()
+         doProduto ()
+         doTipo ()
+         build();
+     
+     
+     }
+     
+     
+     
+}
+     
